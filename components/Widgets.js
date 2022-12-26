4949b89d55605df2a46d76c466ac9473a4da94e1
Kaydenccc <ffadilah682@gmail.com>
2022-12-26 23:06:12 +0800

    install nextauth and firebase and initialize the firebase

--- components/Widgets.js
+++ components/Widgets.js
@@ -6,7 +6,7 @@ const Widgets = ({ news, followers }) => {
   const [articleNum, setArticleNum] = useState(2);
   const [usersNum, setUsersNum] = useState(3);
   return (
-    <div className="xl:w-[350px] md:w-[300px] hidden lg:inline ml-8 space-y-5">
+    <div className="xl:w-[350px] sm:w-[300px] hidden lg:inline ml-8 space-y-5">
       <div className="w-[90%]  sticky top-0 bg-white py-1.5 z-50">
         <div className="flex items-center p-3 relative rounded-full ">
           <MagnifyingGlassIcon className="h-5 z-50 text-gray-500" />
